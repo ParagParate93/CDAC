da023b507fcdc0d649e797c7ced75fbf3dcdabb5
ParagParate93 <paragparate279@gmail.com>
2024-09-12 07:02:52 +0530

    Add files via upload

---
+++ OOPJ/Assig4/Main.java
@@ -0,0 +1,87 @@
+package org.example_Assig4;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+	/*1) Write a program that demonstrates widening conversion from int to double and prints the result.
+	 * 
+		int num = 2379;
+		double d = num;
+		System.out.println(d);
+		
+
+	}
+	
+	*/
+	
+	/*2) Create a program that demonstrates narrowing conversion from double to int and prints the result.
+
+	double d = 4963.6723d;
+	int num = (int)d;
+	System.out.println(num);
+	*/
+	
+	/*3) Write a program that performs arithmetic operations involving different data types (int, double, float) 
+	 * and observes how Java handles widening conversions automatically.
+	
+	double d = 244.22;
+	int num = 204;
+	float f =22.44f;
+	
+	System.out.println(d+num);
+	System.out.println(d+f);
+	System.out.println(f+num);
+	
+	System.out.println(d*num);
+	System.out.println(d*f);
+	System.out.println(f*num);
+	
+	System.out.println(d/num);
+	System.out.println(d/f);
+	System.out.println(f/num);
+	*/
+	
+	/*
+	int num = 65;
+	double d = num;
+	float f = num;
+	String s = Integer.toString(num);
+	
+	
+	System.out.println(d);
+	System.out.println(f);
+	System.out.println(s);
+	
+	//boolean b = num; // can't convert from int to boolean.
+	//System.out.println(b);	
+	*/
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
